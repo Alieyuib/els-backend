ad64a69ea952e55248d69fb0008bf07f5962cd3f
mrceoxyz <mrceoxyz001@gmail.com>
2026-01-21 03:00:54 +0100

    build.sh_v4

--- build.sh
+++ build.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
-# exit on error
+
+# Exit immediately if a command exits with a non-zero status.
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
-# python manage.py collectstatic --no-input
+
+# Make database migrations
 python manage.py makemigrations
+
+# Apply database migrations
 python manage.py migrate
+
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
+  python manage.py createsuperuser --noinput
+fi
